e424e7888f52beddfdc7da0dc297c8265ea2b63e
Luri <luri@kioss.com>
2020-02-16 23:13:59 +0700

    gitignore

--- serverless/amazon/lambda/buildLambdaZip.sh
+++ serverless/amazon/lambda/buildLambdaZip.sh
@@ -3,4 +3,4 @@ rm -rf echoLambda.zip
 cp ../serverless/amazon/lambda/lambda.js .
 zip -r echoLambda.zip lambda.js echo.bin ./config/ ./ztemp/
 rm -rf lambda.js
-
+mv echoLambda.zip ../serverless/amazon/lambda/
